2057e9ee749681c0fa75ee35b8130408d47f4d25
Timothy Gu <timothygu@stanford.edu>
2023-03-02 10:36:29 -0800

    Support code for string

--- etch4/decls_tpch.cpp
+++ etch4/decls_tpch.cpp
@@ -3,19 +3,19 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
+
 #include "sqlite3.h"
 
 constexpr int TPCH_ARRAY_SIZE = 10000000;
 
 // populated later
-int ASIA;
-
 int tpch_lineitem1_pos[TPCH_ARRAY_SIZE];  // orderkey
 int tpch_lineitem1_crd[TPCH_ARRAY_SIZE];
 int tpch_lineitem2_pos[TPCH_ARRAY_SIZE];  // suppkey
 int tpch_lineitem2_crd[TPCH_ARRAY_SIZE];
 double tpch_lineitem_vals[TPCH_ARRAY_SIZE];
-  // supposed to be l_extendedprice * (1 - l_discount)
+// supposed to be l_extendedprice * (1 - l_discount)
 
 // tpch_customer1 = custkey (dense)
 int tpch_customer2_pos[TPCH_ARRAY_SIZE];  // nationkey
@@ -32,6 +32,10 @@ int tpch_supplier2_crd[TPCH_ARRAY_SIZE];
 // tpch_nation1 = nationkey (dense)
 int tpch_nation2_pos[TPCH_ARRAY_SIZE];  // regionkey
 int tpch_nation2_crd[TPCH_ARRAY_SIZE];
+
+// tpch_region1 = regionkey (dense)
+int tpch_region2_pos[TPCH_ARRAY_SIZE];  // regionkey
+char* tpch_region2_crd[TPCH_ARRAY_SIZE];
 
 // this is the "mat" type, aka "dcsr"
 #define GEN_MAT(tbl_name)                                                  \
@@ -62,7 +66,7 @@ int tpch_nation2_crd[TPCH_ARRAY_SIZE];
   }
 
 // this is the "dsTbl2" type, aka "csr" but without values
-#define GEN_DSTBL2(tbl_name)                                                   \
+#define GEN_DSTBL2(tbl_name, crd2eq, crd2conv, crd2copy)                       \
   static int tbl_name##_i1_ = 0;                                               \
   static int gen_##tbl_name##_callback(void* data, int argc, char** argv,      \
                                        char** azColName) {                     \
@@ -72,53 +76,54 @@ int tpch_nation2_crd[TPCH_ARRAY_SIZE];
     }                                                                          \
     if (tbl_name##2_pos [atoi(argv[0])] ==                                     \
             tbl_name##2_pos [atoi(argv[0]) + 1] ||                             \
-        atoi(argv[1]) !=                                                       \
-            tbl_name##2_crd [tbl_name##2_pos [atoi(argv[0]) + 1] - 1]) {       \
+        !crd2eq(crd2conv(argv[1]),                                             \
+                tbl_name##2_crd [tbl_name##2_pos [atoi(argv[0]) + 1] - 1])) {  \
       tbl_name##2_pos [atoi(argv[0]) + 1] += 1;                                \
     }                                                                          \
-    tbl_name##2_crd [tbl_name##2_pos [atoi(argv[0]) + 1] - 1] = atoi(argv[1]); \
+    tbl_name##2_crd [tbl_name##2_pos [atoi(argv[0]) + 1] - 1] =                \
+        crd2copy(argv[1]);                                                     \
                                                                                \
     return 0;                                                                  \
   }
 
 // this is the "ssTbl2" type, aka "dcsr" but without values
-#define GEN_SSTBL2(tbl_name)                                               \
-  static int gen_##tbl_name##_callback(void* data, int argc, char** argv,  \
-                                       char** azColName) {                 \
-    if (tbl_name##1_pos [0] == tbl_name##1_pos [1] ||                      \
-        atoi(argv[0]) != tbl_name##1_crd [tbl_name##1_pos [1] - 1]) {      \
-      tbl_name##1_pos [1] = (tbl_name##1_pos [1] + 1);                     \
-      tbl_name##2_pos [((tbl_name##1_pos [1] - 1) + 1)] =                  \
-          tbl_name##2_pos [(tbl_name##1_pos [1] - 1)];                     \
-    }                                                                      \
-    tbl_name##1_crd [tbl_name##1_pos [1] - 1] = atoi(argv[0]);             \
-                                                                           \
-    if (tbl_name##2_pos [tbl_name##1_pos [1] - 1] ==                       \
-            tbl_name##2_pos [tbl_name##1_pos [1]] ||                       \
-        atoi(argv[1]) !=                                                   \
-            tbl_name##2_crd [tbl_name##2_pos [tbl_name##1_pos [1]] - 1]) { \
-      tbl_name##2_pos [tbl_name##1_pos [1]] += 1;                          \
-    }                                                                      \
-                                                                           \
-    tbl_name##2_crd [tbl_name##2_pos [tbl_name##1_pos [1]] - 1] =          \
-        atoi(argv[1]);                                                     \
-                                                                           \
-    return 0;                                                              \
+#define GEN_SSTBL2(tbl_name, crd1eq, crd1conv, crd1val, crd2eq, crd2conv,   \
+                   crd2copy)                                                \
+  static int gen_##tbl_name##_callback(void* data, int argc, char** argv,   \
+                                       char** azColName) {                  \
+    if (tbl_name##1_pos [0] == tbl_name##1_pos [1] ||                       \
+        !crd1eq(crd1conv(argv[0]),                                          \
+                tbl_name##1_crd [tbl_name##1_pos [1] - 1])) {               \
+      tbl_name##1_pos [1] = (tbl_name##1_pos [1] + 1);                      \
+      tbl_name##2_pos [((tbl_name##1_pos [1] - 1) + 1)] =                   \
+          tbl_name##2_pos [(tbl_name##1_pos [1] - 1)];                      \
+    }                                                                       \
+    tbl_name##1_crd [tbl_name##1_pos [1] - 1] = crd1copy(argv[0]);          \
+                                                                            \
+    if (tbl_name##2_pos [tbl_name##1_pos [1] - 1] ==                        \
+            tbl_name##2_pos [tbl_name##1_pos [1]] ||                        \
+        !crd2eq(                                                            \
+            crd2conv(argv[1]),                                              \
+            tbl_name##2_crd [tbl_name##2_pos [tbl_name##1_pos [1]] - 1])) { \
+      tbl_name##2_pos [tbl_name##1_pos [1]] += 1;                           \
+    }                                                                       \
+                                                                            \
+    tbl_name##2_crd [tbl_name##2_pos [tbl_name##1_pos [1]] - 1] =           \
+        crd2copy(argv[1]);                                                  \
+                                                                            \
+    return 0;                                                               \
   }
 
-GEN_MAT(tpch_lineitem)
-GEN_DSTBL2(tpch_customer)
-GEN_DSTBL2(tpch_orders)
-GEN_DSTBL2(tpch_supplier)
-GEN_DSTBL2(tpch_nation)
+#define EQ(a, b) (a == b)
+#define STR_EQ(a, b) (strcmp(a, b) == 0)
+#define ID(a) (a)
 
-static int gen_ASIA_callback(void* data,
-                             int argc,
-                             char** argv,
-                             char** azColName) {
-  ASIA = atoi(argv[0]);
-  return 0;
-}
+GEN_MAT(tpch_lineitem)
+GEN_DSTBL2(tpch_customer, EQ, atoi, atoi)
+GEN_DSTBL2(tpch_orders, EQ, atoi, atoi)
+GEN_DSTBL2(tpch_supplier, EQ, atoi, atoi)
+GEN_DSTBL2(tpch_nation, EQ, atoi, atoi)
+GEN_DSTBL2(tpch_region, STR_EQ, ID, strdup)
 
 namespace {
 
@@ -130,7 +135,7 @@ int populate_tpch(sqlite3* db) {
 #define GET_MAT(tbl_name, col1, col2, col3)                                  \
   rc = sqlite3_exec(db,                                                      \
                     "SELECT " #col1 ", " #col2 ", " #col3 " FROM " #tbl_name \
-                    " ORDER BY " #col1 ", " #col2 "",           \
+                    " ORDER BY " #col1 ", " #col2 "",                        \
                     gen_tpch_##tbl_name##_callback, (void*)data, &zErrMsg);  \
   if (rc != SQLITE_OK) {                                                     \
     printf("%s:%d: %s\n", __FILE__, __LINE__, zErrMsg);                      \
@@ -147,20 +152,12 @@ int populate_tpch(sqlite3* db) {
     return rc;                                                              \
   }
 
-  rc = sqlite3_exec(db, "SELECT r_regionkey FROM region WHERE r_name = 'ASIA'",
-                    gen_ASIA_callback, (void*)data, &zErrMsg);
-  if (rc != SQLITE_OK) {
-    printf("%s:%d: %s\n", __FILE__, __LINE__, zErrMsg);
-    return rc;
-  }
-
-  printf("ASIA = %d\n", ASIA);
-
   GET_MAT(lineitem, l_orderkey, l_suppkey, l_extendedprice * (1 - l_discount))
   GET_TBL2(customer, c_custkey, c_nationkey)
   GET_TBL2(orders, o_orderkey, o_custkey)
   GET_TBL2(supplier, s_suppkey, s_nationkey)
   GET_TBL2(nation, n_nationkey, n_regionkey)
+  GET_TBL2(region, r_regionkey, r_name)
 
   return rc;
 }
